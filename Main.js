8260d7d15d0c43afef8d506b2d84768fb6c822df
Jai Shiv Tomar <jaishivthakur56@gmail.com>
2025-06-28 20:14:46 +0530

    Upload full portfolio website with images

---
+++ Main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+   strings: [" Aspiring Developer","Java Coder","Frontend Learner","DSA Problem Solver"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+})
